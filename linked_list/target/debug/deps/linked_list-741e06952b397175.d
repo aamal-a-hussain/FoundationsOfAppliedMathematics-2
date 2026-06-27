496edfda8e9f65f496be007ec2fe168083e16283
Aamal Hussain <aamal@latent-technology.com>
2026-06-27 12:45:13 +0100

    remove target and add to gitignore

--- linked_list/target/debug/deps/linked_list-741e06952b397175.d
+++
@@ -1,6 +0,0 @@
-/home/aamalh/RustroverProjects/FAM2/linked_list/target/debug/deps/linked_list-741e06952b397175.d: src/lib.rs src/doubly_linked_list.rs
-
-/home/aamalh/RustroverProjects/FAM2/linked_list/target/debug/deps/liblinked_list-741e06952b397175.rmeta: src/lib.rs src/doubly_linked_list.rs
-
-src/lib.rs:
-src/doubly_linked_list.rs:
